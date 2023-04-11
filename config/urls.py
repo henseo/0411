ca1fca8a832d7662d0d3c534cdbaeaa8a5152164
Hyeonseo Jeong <jhyeonseo0523@gmail.com>
2023-04-11 12:48:37 +0900

    로컬 폴더로 전체를 업데이트 합니다.

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls')),
+    path('posts/', include('posts.urls')),  # posts/ 추가 -> ~/post/1 이렇게 RestFul!
 ]
